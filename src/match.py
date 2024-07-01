03a1b4306505228e30ab95ab6bf2ec488d6cfab6
Sascha <GlancingMind@outlook.com>
2024-07-01 10:54:20 +0200

    Remove old matches function in match.py

--- src/match.py
+++ src/match.py
@@ -1,53 +1,13 @@
-from collections import ChainMap
 
 def matches(ds: list, pattern: list):
     if pattern == [] and ds == []:
         return [{}]
     if pattern == []:
         return ["f"]
-
     if type(ds) != type(pattern):
         return ["f"]
 
-    m = []
-    matcher, *rstPat = pattern
-    match matcher:
-        case list():
-            word, *rstData = ds
-            m += matches(rstData, rstPat) + matches(word, matcher)
-            if "f" in m:
-                return ["f"]
-            return [dict(ChainMap(*m))]
-        case str() if matcher.startswith('@'):
-            return [{ matcher: ds }]
-        case str():
-            if ds == []:
-                return ["f"]
-            # nil ist ein Wort und wird hier aufgespalten! Das ist ein problem.
-            word, *rstData = ds
-            if matcher.startswith('#'):
-                m +=  matches(rstData, rstPat) + [{matcher: word}]
-            elif word != matcher:
-                return ["f"]
-            elif word == matcher:
-                m += matches(rstData, rstPat) + [{}]
-
-    if "f" in m:
-        return ["f"]
-    if matcher in m[0].keys() and m[0][matcher] != word:
-        return ["f"]
-    return [dict(ChainMap(*m))]
-
-
-def nm(ds: list, pattern: list):
-    if pattern == [] and ds == []:
-        return [{}]
-    if pattern == []:
-        return ["f"]
-    if type(ds) != type(pattern):
-        return ["f"]
-
-    matches = {}
+    foundMatches = {}
     tuples = []
     popIdx = 0
     containsAtMatcher = False
@@ -72,47 +32,47 @@ def nm(ds: list, pattern: list):
                 if matcher != e:
                     return ["f"]
             case list():
-                m = nm(ds.pop(popIdx), matcher)
+                m = matches(ds.pop(popIdx), matcher)
                 for k,v in m[0].items():
-                    if matches.get(k, v) != v:
+                    if foundMatches.get(k, v) != v:
                         return ["f"]
-                    matches[k] = v
+                    foundMatches[k] = v
 
     if ds != [] and not containsAtMatcher:
         return ["f"]
 
     for t in tuples:
         k,v = t
-        if matches.get(k, v) != v:
+        if foundMatches.get(k, v) != v:
             return ["f"]
-        matches[k] = v
+        foundMatches[k] = v
 
-    return [matches]
+    return [foundMatches]
 
-assert nm([{}],[{}]) == [{}], " "
-assert nm([{"1":"Hello"}],[{"1":"Hello"}]) == [{}], " "
-assert nm([{"1":"Hello"}],[{"1":"World"}]) == ["f"], " "
-assert nm([{"2":"Hello"}],[{"1":"Hello"}]) == ["f"], " "
-assert nm(["1","2","3","4"],["#F","#S","@M","#L"]) == [{"#F": "1", "#S": "2", "@M": ["3"], "#L": "4"}], ""
-assert nm(["1","2","3","4","5"],["1","2","@T","#L"]) == [{"#L": "5", "@T": ["3","4"]}], ""
-assert nm([],[]) == [{}], " "
-assert nm(["1","2","3","4"],["1","2","3","4"]) == [{}], ""
-assert nm(["1","2","3","4"],["1","2","3","1"]) == ["f"], ""
-assert nm(["1","2","3","4"],["1","2","3"]) == ["f"], ""
-assert nm(["1","2","3","4"],["@T"]) == [{"@T": ["1","2","3","4"]}], ""
-assert nm([],["@T"]) == [{"@T": []}], ""
-assert nm([],["#H"]) == ["f"], ""
-assert nm([],["#H", "@T"]) == ["f"], ""
-assert nm(["1"],["#F"]) == [{ "#F": "1"}], ""
-assert nm(["1"],["#F", "#S"]) == ["f"], ""
-assert nm(["1"],["#F", "@R"]) == [{"#F": "1", "@R": []}], ""
-assert nm(["1",["2","3"], "4"],["#X", "#Y", "#Z"]) == [{"#X": "1", "#Y": ["2", "3"], "#Z": "4"}], ""
-assert nm(["1",["2","3"], "4"],["#X", "@Y"]) == [{"#X": "1", "@Y": [["2", "3"], "4"]}], ""
-assert nm(["1",["2","3"], "4"],["#X", "#Y", "#Z", "#U"]) == ["f"], ""
-assert nm(["1","2","3"],["#X", "#Y", "#X"]) == ["f"], ""
-assert nm(["1","2","1"],["#X", "#Y", "#X"]) == [{"#X": "1", "#Y": "2"}], ""
-assert nm([["1",{"2": "3"},"4","5"],"6","7"],[["#F", "#S", "@R"], "@T"]) == [{ "#F": "1", "#S": { "2": "3" }, "@R": [ "4", "5" ], "@T": [ "6", "7" ] }], ""
-assert nm([["1",{"2": "3"},"4","5"],"6","7"],["#1", "@2"]) == [{ "#1": ["1", { "2": "3" }, "4", "5" ], "@2": [ "6", "7" ] }], ""
+assert matches([{}],[{}]) == [{}], " "
+assert matches([{"1":"Hello"}],[{"1":"Hello"}]) == [{}], " "
+assert matches([{"1":"Hello"}],[{"1":"World"}]) == ["f"], " "
+assert matches([{"2":"Hello"}],[{"1":"Hello"}]) == ["f"], " "
+assert matches(["1","2","3","4"],["#F","#S","@M","#L"]) == [{"#F": "1", "#S": "2", "@M": ["3"], "#L": "4"}], ""
+assert matches(["1","2","3","4","5"],["1","2","@T","#L"]) == [{"#L": "5", "@T": ["3","4"]}], ""
+assert matches([],[]) == [{}], " "
+assert matches(["1","2","3","4"],["1","2","3","4"]) == [{}], ""
+assert matches(["1","2","3","4"],["1","2","3","1"]) == ["f"], ""
+assert matches(["1","2","3","4"],["1","2","3"]) == ["f"], ""
+assert matches(["1","2","3","4"],["@T"]) == [{"@T": ["1","2","3","4"]}], ""
+assert matches([],["@T"]) == [{"@T": []}], ""
+assert matches([],["#H"]) == ["f"], ""
+assert matches([],["#H", "@T"]) == ["f"], ""
+assert matches(["1"],["#F"]) == [{ "#F": "1"}], ""
+assert matches(["1"],["#F", "#S"]) == ["f"], ""
+assert matches(["1"],["#F", "@R"]) == [{"#F": "1", "@R": []}], ""
+assert matches(["1",["2","3"], "4"],["#X", "#Y", "#Z"]) == [{"#X": "1", "#Y": ["2", "3"], "#Z": "4"}], ""
+assert matches(["1",["2","3"], "4"],["#X", "@Y"]) == [{"#X": "1", "@Y": [["2", "3"], "4"]}], ""
+assert matches(["1",["2","3"], "4"],["#X", "#Y", "#Z", "#U"]) == ["f"], ""
+assert matches(["1","2","3"],["#X", "#Y", "#X"]) == ["f"], ""
+assert matches(["1","2","1"],["#X", "#Y", "#X"]) == [{"#X": "1", "#Y": "2"}], ""
+assert matches([["1",{"2": "3"},"4","5"],"6","7"],[["#F", "#S", "@R"], "@T"]) == [{ "#F": "1", "#S": { "2": "3" }, "@R": [ "4", "5" ], "@T": [ "6", "7" ] }], ""
+assert matches([["1",{"2": "3"},"4","5"],"6","7"],["#1", "@2"]) == [{ "#1": ["1", { "2": "3" }, "4", "5" ], "@2": [ "6", "7" ] }], ""
 
 # % test instantiate
 # ( [ 1 2 3 ] ) [ { #H 1 @T [ 2 3 ] } [ #H @T ] instantiate ] unit-test
@@ -121,3 +81,7 @@ assert nm([["1",{"2": "3"},"4","5"],"6","7"],["#1", "@2"]) == [{ "#1": ["1", { "
 # % test rewrite
 # ( [ y x z u v ] ) [ [ x y z u v ] [ #F #S @R ] [ #S #F @R ] rewrite ] unit-test
 # ( f ) [ [ x ] [ #F #S @R ] [ #S #F @R ] rewrite ] unit-test
+
+# TODO Add instantiate to this module
+# TODO Update Parser
+# TODO should matchers also exist for dictionary content? E.g. { #X #Y @RI }
