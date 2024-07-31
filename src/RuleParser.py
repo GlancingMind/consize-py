9240c34f95122131a0b4e06355132ed9e556816f
Sascha <GlancingMind@outlook.com>
2024-07-31 13:31:17 +0200

    Fix parsing of nested Patterns in dict
    ...hopefully once and for all...

--- src/RuleParser.py
+++ src/RuleParser.py
@@ -9,65 +9,82 @@ class RuleParser:
         i_ds_pat, i_cs_pat = self.__parseRightRuleSide(rhs)
         return Rule(mp=m_ds_pat, ocs= m_cs_pat, ip=i_ds_pat, ncs=i_cs_pat)
 
-    def __parseLeftRuleSide(self, str: str):
-        *dspStr, cspStr = re.split(r"\s*\|\s+", str)
-        dsp = self.__parsePattern(dspStr[0] if dspStr != [] else "")
-        csp = self.__parsePattern(cspStr, isCallstack=True)
+    def __parseLeftRuleSide(self, sideStr: str):
+        *dspStr, cspStr = re.split(r"\s*\|\s+", sideStr)
+        dsp = self.__parsePattern(re.split(r"\s+", dspStr[0] if dspStr != [] else ""))
+        csp = self.__parsePattern(re.split(r"\s+", cspStr))
+
+        if all(word == "" for word in dsp if isinstance(word, str)):
+            dsp = []
+
+        if all(word == "" for word in csp if isinstance(word, str)):
+            csp = []
+
+        if not any(word.startswith("@") for word in dsp if isinstance(word, str)):
+            dsp = ["@RDS"] + dsp
+
+        if not any(word.startswith("@") for word in csp if isinstance(word, str)):
+            csp = ["@RCS"] + csp
+        csp.reverse()
+
         return dsp, csp
 
-    def __parseRightRuleSide(self, str: str):
-        dspStr, *cspStr = re.split(r"\s*\|\s+", str)
-        dsp = self.__parsePattern(dspStr)
-        csp = self.__parsePattern(cspStr[0] if cspStr != [] else "", isCallstack=True)
+    def __parseRightRuleSide(self, sideStr: str):
+        dspStr, *cspStr = re.split(r"\s*\|\s+", sideStr)
+        dsp = self.__parsePattern(re.split(r"\s+", dspStr))
+        csp = self.__parsePattern(re.split(r"\s+", cspStr[0] if cspStr != [] else ""))
+
+        if all(word == "" for word in dsp if isinstance(word, str)):
+            dsp = []
+
+        if all(word == "" for word in csp if isinstance(word, str)):
+            csp = []
+
+        if not any(word.startswith("@") for word in dsp if isinstance(word, str)):
+            dsp = ["@RDS"] + dsp
+
+        if not any(word.startswith("@") for word in csp if isinstance(word, str)):
+            csp = ["@RCS"] + csp
+        csp.reverse()
+
         return dsp, csp
 
-    def __parsePattern(self, str: str, isCallstack=False):
-        tokens = re.split(r"\s+", str)
-
-        addRestMatcher = True
+    def __parsePattern(self, tokens: list[str]):
         pattern = []
-        popIdx = 0
-        while tokens != []:
-            token = tokens.pop(popIdx)
-            if token == "[":
-                pattern.append(self.parseStack(tokens))
-            elif token == "{":
-                pattern.append(self.parseDict(tokens))
-            elif token != "":
-                if addRestMatcher:
-                    addRestMatcher = not token.startswith('@')
-                pattern.append(token)
-
-        if isCallstack:
-            pattern.reverse()
-
-        if addRestMatcher:
-            pattern = ["@RCS" if isCallstack else "@RDS"] + pattern
-
-        return pattern
-
-    def parseStack(self, tokens: list):
-        stack = []
         while tokens != []:
             token = tokens.pop(0)
+            if token == "[":
+                pattern.append(self.__parsePattern(tokens))
             if token == "]":
-                return stack
-            elif token == "[":
-                stack.append(self.parseStack(tokens))
+                return pattern
+            if token == "{":
+                pattern.append(self.__parseDict(tokens))
             else:
-                stack.append(token)
+                pattern.append(token)
+        return pattern
 
-        return stack
-
-    def parseDict(self, tokens: list):
-        stack = Dictionary()
+    def __parseDict(self, tokens: list[str]):
+        d = dict()
+        key = None
         while tokens != []:
             token = tokens.pop(0)
             if token == "{":
-                stack.append(self.parseDict(tokens))
+                d.append(self.__parseDict(tokens))
             if token == "}":
                 break
+            elif key == None:
+                key = token
             else:
-                stack.append(token)
+                value = self.__parsePattern([token])
+                # The result of __parsePattern will be always a stack.
+                # Therefore __parsePattern([b]) for { a b }, will return [b].
+                # But what we want is the unwrapped b, therefore we unwrap it.
+                # What if { a [ b ] } is given?
+                # Then __parsePattern will return
+                # [[b]], which will be unwrapped, yielding the correct value [b].
+                if len(value) == 1:
+                    d[key] = value[0]
+                else:
+                    d[key] = value
 
-        return stack
+        return d
