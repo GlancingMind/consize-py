47f28f66d865eee546dd2bce07e4dcd0756c840b
Sascha <GlancingMind@outlook.com>
2024-08-02 17:43:38 +0200

    Fix printing of maps

--- src/Stack.py
+++ src/Stack.py
@@ -1,9 +1,9 @@
-class Dictionary(list):
+class Stack(list):
     def __init__(self, *args):
         super().__init__(args)
 
     def __repr__(self):
-        return (" ").join(["{",*self,"}"])
+        return (" ").join(["[",*self,"]"])
 
     def copy(self) -> list:
-        return Dictionary(super().copy())
+        return Stack(super().copy())
--- src/StackSerializer.py
+++ src/StackSerializer.py
@@ -1,12 +1,3 @@
-from Dictionary import Dictionary
 
-def stringify_stack(lst, printStackParanthesis=False):
-    if lst == []:
-        return "[ ]"
-    s = ' '.join(stringify_stack(item, True) if isinstance(item, list) else str(item) for item in lst)
-    if printStackParanthesis == True:
-        if isinstance(lst, Dictionary):
-            s = "{ "+s+" }"
-        else:
-            s = "[ "+s+" ]"
-    return s
+def stringify_stack(lst: list, printStackParanthesis=False):
+    return (" ").join([str(itm) for itm in lst])
